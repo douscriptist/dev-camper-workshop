f3c2365e4bcee657ad109050a86f95f65aa2546b
Kaan Yilmaz <douscriptist@gmail.com>
2020-04-20 15:10:04 +0300

    custom middleware

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
 
 // Load env file
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Middlawares
+app.use(logger);
 
 // ROUTES
 app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
