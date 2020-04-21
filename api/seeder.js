3fa0efdb114c97b65d42c1a3a22deeda7503d70c
Kaan Yilmaz <douscriptist@gmail.com>
2020-04-21 14:14:04 +0300

    basic bootcamp seeder

---
+++ api/seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env file & variables
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect Database
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/source/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import into Database
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+
+		console.log('Data Imported...'.green.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// Delete Data
+const deleteData = async () => {
+	try {
+		await Bootcamp.deleteMany();
+
+		console.log('Data Destroyed...'.red.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+if (process.argv[2] === '-import') {
+	importData();
+} else if (process.argv[2] === '-destroy') {
+	deleteData();
+}
