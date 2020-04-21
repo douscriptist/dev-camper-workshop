757f5c0e8c67cf98ef911e823f0c8828eb02d831
Kaan Yilmaz <douscriptist@gmail.com>
2020-04-21 19:39:24 +0300

    pagination

--- api/controllers/bootcamps.js
+++ api/controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	const reqQuery = { ...req.query };
 
 	// Fields to exclude
-	const removeFields = ['select', 'sort'];
+	const removeFields = ['select', 'sort', 'page', 'limit'];
 
 	// Loop through removeFields & Delete from reqQuery
 	removeFields.forEach((param) => delete reqQuery[param]);
@@ -45,13 +45,40 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 		query = query.sort('-createdAt');
 	}
 
+	// Pagination & Limiting
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 20;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+
+	query = query.skip(startIndex).limit(limit);
+
 	// Exec query
 	const bootcamps = await query;
+
+	// Pagination Results
+	const pagination = {};
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		};
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		};
+	}
 
 	res.status(200).json({
 		success: true,
 		msg: 'Show all bootcamps.',
 		count: bootcamps.length,
+		pagination,
 		data: bootcamps,
 	});
 });
