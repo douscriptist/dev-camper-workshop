0fdea468aa8123cede8d2dfb69fc96503aa232c6
Kaan Yilmaz <douscriptist@gmail.com>
2020-04-21 03:14:43 +0300

    handle duplicate error

--- api/middleware/error.js
+++ api/middleware/error.js
@@ -5,12 +5,19 @@ const errorHandler = (err, req, res, next) => {
 	error.message = err.message;
 
 	// Log to console for dev
-	console.log(err.stack.red);
+	// console.log(err.stack.red);
+	console.log(err);
 
 	// Mongoose bad ObjectId - CastError
 	if (err.name === 'CastError') {
-		const message = `Resource not found with id of ${err.value}`;
+		const message = `Resource not found with id of ${err.value}.`;
 		error = new ErrorResponse(message, 404);
+	}
+
+	// Mongoose duplicate key - MongoError - 11000
+	if (err.code === 11000) {
+		const message = 'Duplicate field value entered.';
+		error = new ErrorResponse(message, 400);
 	}
 
 	res.status(error.statusCode || 500).json({
